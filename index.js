ea2eb44b0c5a555b47d6cc1722f144afcb410219
Mehedi Hasan <mehedihasan624358@gmail.com>
2025-11-09 22:51:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('helo')
+    console.log('home')
+})
+
+const PORT = process.env.PORT || 3000;
+console.log("Will listen on PORT =", PORT);
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
